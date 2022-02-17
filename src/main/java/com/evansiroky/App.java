6e7cac882f0f06c5ba0af7284773d3a40ceb09be
xzima <xzima@ro.ru>
2022-02-18 01:42:09 +0300

    fix: change App.java

--- src/main/java/com/evansiroky/App.java
+++ src/main/java/com/evansiroky/App.java
@@ -2,12 +2,13 @@ package com.evansiroky;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException();
     }
 }
