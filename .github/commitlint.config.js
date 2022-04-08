9c0857e7ed1e184bc81bf9cf5010bd690034331b
Alex Zima <xzima@ro.ru>
2022-04-08 03:00:18 +0300

    fix(ci): ignore ci skip commit verification

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -3,7 +3,8 @@ const Configuration = {
     // https://commitlint.js.org/#/reference-rules
     rules: {
         'body-max-line-length': [2, 'always', 150],
-    }
+    },
+    ignores: [(commit) => ['[skip ci]', '[skip ci]', '[no ci]'].some(it => commit.includes(it))],
 };
 
 module.exports = Configuration;
